537e29d1a9293395eafbac31c799d4c46950e896
root <519100949@qq.com>
2016-04-18 11:27:51 +0800

    1

--- src/main/java/com/practice/HelloController.java
+++ src/main/java/com/practice/HelloController.java
@@ -11,14 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-//    捕获 /hello 请求，利用hello(Model model) 处理请求，并返回‘hello’，交给Thymeleaf 处理
+    //    捕获 /hello 请求，利用hello(Model model) 处理请求，并返回‘hello’，交给Thymeleaf 处理
 //    hello 对应html文件名。model 作为信息的载体，封装各类变量、对象
-    @RequestMapping("/hello")
-    public String hello(Model model) {
+    @RequestMapping("/")
+    public String index(Model model) {
         String name = "Spring4 MVC";
 
         model.addAttribute("name", name);
 
-        return "hello";
+        return "index";
+
     }
 }
